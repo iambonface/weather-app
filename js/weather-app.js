e88d129326a17214beeb8e1b0051a0ee4f28cb53
Bonface Mudiri <mudirib@gmail.com>
2017-06-10 20:20:45 +0300

    Update index.html to toggle temperature conversation and create on click function to convert from degrees to fahreinheit

--- js/weather-app.js
+++ js/weather-app.js
@@ -1,12 +1,47 @@
 $(document).ready(function(){
 
+	$("i").css({
+		"display": "none",
+		"transition": "all ease 0.8s"
+	});
 
-/*
-	getForecast();
+	$(".fa-free-code-camp").css({
+		"display": "inline"
+	})
+	$(".fa-heart").css({
+		"display": "inline"
+	})
+	$(".fa-map-marker").css({
+		"display": "inline"
+	})
+
+	$("#Ping").click(function(){
+		getForecast();
+	});
+
 
 	function getForecast(){
 
 		navigator.geolocation.getCurrentPosition(function(position){
+		
+		$("i").css({
+			"display": "block"
+		});
+
+		$(".fa-free-code-camp").css({
+			"display": "inline"
+		})
+		$(".fa-heart").css({
+			"display": "inline"
+		})
+
+		$("#ShareText").css("display", "none");
+
+		$("#Degrees").css("display", "inline");
+
+
+
+
    	   	//console.log(position);
    	   	console.log(position.coords.longitude);
    	   	var coordinate = position.coords.latitude + ',' + position.coords.longitude;
@@ -23,12 +58,77 @@ $(document).ready(function(){
 				
 				//console.log(value.currently.temperature.toFixed(2));
 
-				var celsiusValue = (value.currently.temperature - 32) * (5/9);
+				
 
-				console.log("Zone: " + value.timezone);
-				console.log("Temperature: " + celsiusValue.toFixed(2) + " celsius");
-				console.log("Current Status: " + value.currently.icon)
-				console.log("Forecast: "+ value.hourly.summary + value.daily.summary)
+				console.log(value);
+
+
+			    //console.log(value.currently.temperature.toFixed(2));
+			    $("#Temperature").html(value.currently.temperature.toFixed(2) + "&#8457");
+
+
+			    //console.log("Temperature: " + celsiusValue.toFixed(2) + " celsius");
+				//Convert to Degrees Celsius
+				$("#Degrees").click(function(){
+
+					if($(this).attr('data-click-state') == 1){
+						$(this).attr('data-click-state', 0)
+						$(this).css({
+							"background-color": "#7b241c",
+							"border": "none"
+						})
+						$('.fa-thermometer-half').css('color', "#7b241c")
+						$(this).text('Change to Degrees Fahreinheit')
+						
+					var celsiusValue = (value.currently.temperature - 32) * (5/9);
+					$("#Temperature").html(celsiusValue.toFixed(2) + "&#8451");
+					} else{
+						$(this).attr('data-click-state', 1)
+						$(this).css("background-color", "orange")
+						$('.fa-thermometer-half').css('color', 'orange')
+						$(this).text('Change to Degrees Celsius')
+
+					var fahreinheit = (value.currently.temperature);
+					$("#Temperature").html(fahreinheit.toFixed(2) + "&#8457");
+					}
+
+				});
+
+
+    			//console.log("Current Status: " + value.currently.icon);
+    			//$("#CurrentIcon").html(value.currently.icon);
+
+    			//Tweak result to display with first letter capitalized
+    			var currentForecastArr = [];
+    			var str = value.currently.icon;
+    			var arr = str.split('');
+    			var newArr = [];
+
+    			for(var i in arr){
+    				if(arr[i] === "-"){
+    					arr[i] = " ";
+    				}
+
+    				newArr.push(arr[i]);
+    			}
+
+    			var newStr = newArr.join('');
+    			$("#CurrentIcon").html(newStr);
+
+
+
+
+				//console.log("Forecast: "+ value.hourly.summary + value.daily.summary)
+				$("#Summary").html(value.hourly.summary + " " + value.daily.summary);
+
+
+				//Humidity
+			    $("#Humidity").html(((value.currently.humidity)*100).toFixed(2) + " %");
+			    //Humidity
+			    $("#Pressure").html(value.currently.pressure + " hPa");
+			    //Humidity
+			    $("#WindSpeed").html(value.currently.windSpeed + " m/h");
+
 
 			});
 			
@@ -41,7 +141,9 @@ $(document).ready(function(){
 		$.getJSON(mapRoot, function(address){
 			$(address).each(function(i, v){
 				var address = v.results[i].formatted_address;
-				console.log(address);
+				//console.log(address);
+				$("#Address").html(address);
+
 			});
 
 		});
@@ -52,7 +154,6 @@ $(document).ready(function(){
 
 	} //end of getForecast
 
+
 	
 }); //end of document.ready()
-
-*/
